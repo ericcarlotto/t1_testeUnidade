f0e59f01758720177a49df8e508d308279eea7a2
henriquewwf <61994998+henriquewwf@users.noreply.github.com>
2022-04-10 18:07:53 -0300

    Create test_2_Henrique.java
    Add TestCase 2 for beecrowd 1019: 1s.

---
+++ src/test/java/test_2_Henrique.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class test_2_Henrique {
+    @Test
+    public void entrada_1_saida_0_0_1() {
+        assertEquals("0:0:1", Relogio.calcula(1));
+    }
+}  
