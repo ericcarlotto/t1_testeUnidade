bf08f5f197276be1a1012f6ae941dd728e34aa33
henriquewwf <61994998+henriquewwf@users.noreply.github.com>
2022-04-11 21:36:46 -0300

    Correção Classe Teste
    Alterado Nome Classe para ser identico ao arquivo.

--- src/test/java/tet_3_Matheus.java
+++ src/test/java/tet_3_Matheus.java
@@ -1,7 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class test_carlottoEric {
+public class tet_3_Matheus{
     @Test
     public void entrada_140153_saida_38_55_53() {
         assertEquals("38:55:53", Relogio.calcula(140153));
